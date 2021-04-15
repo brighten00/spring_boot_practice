249006379503f2e978398fa75173e24baf9aae99
brighten00 <brighten00@gmail.com>
2021-04-15 17:54:23 +0800

    add @Service
    fix annotation error
    add @ Service

--- src/main/java/com/wei/spring_boot_practice/service/ProductService.java
+++ src/main/java/com/wei/spring_boot_practice/service/ProductService.java
@@ -6,9 +6,11 @@ import com.wei.spring_boot_practice.exception.NotFoundException;
 import com.wei.spring_boot_practice.prameter.ProductQueryParameter;
 import com.wei.spring_boot_practice.repository.MockProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductService {
     @Autowired
     private MockProductDAO productDAO;
