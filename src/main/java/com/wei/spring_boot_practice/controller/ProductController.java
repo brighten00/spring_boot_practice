4eccb9574bd25043931e1e46418696abb0f368a8
brighten00 <brighten00@gmail.com>
2021-04-15 17:31:24 +0800

    20210415 add service layer and move simulation DB out from controller
    add service layer and move simulation DB out from controller

--- src/main/java/com/wei/spring_boot_practice/controller/ProductController.java
+++ src/main/java/com/wei/spring_boot_practice/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.wei.spring_boot_practice.controller;
 
 import com.wei.spring_boot_practice.entity.Product;
 import com.wei.spring_boot_practice.prameter.ProductQueryParameter;
+import com.wei.spring_boot_practice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,49 +20,24 @@ import java.util.stream.Collectors;
 @RestController
 //set "/products" as default path
 @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
 
-//    database simulation
-    private final List<Product> productDB = new ArrayList<>();
+    @Autowired
+    private ProductService productService;
 
-    @PostConstruct
-    public void initDB(){
-        productDB.add(new Product("T0001","Trading Holy Grail", 200));
-        productDB.add(new Product("T0002","Trading FX", 180));
-        productDB.add(new Product("T0003","Finance Management", 360));
-        productDB.add(new Product("T0004","Guns, Germs, and Steel", 540));
-        productDB.add(new Product("T0005","Billion Whale", 320));
-    }
 //  GET "/products/{id}"  get product data
 //  can be replace by @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable("id") String id){
-        Optional<Product> productOp = productDB.stream().filter(p -> p.getId().equals(id)).findFirst();
-
-        if(!productOp.isPresent()){
-            return ResponseEntity.notFound().build();
-        }
-
-        Product product = productOp.get();
-
+        Product product = productService.getProduct(id);
         return ResponseEntity.ok().body(product);
     }
 
 //  POST "/products" create new product
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product request) {
-        boolean isIdDuplicated = productDB.stream().anyMatch(p -> p.getId().equals(request.getId()));
-
-        if (isIdDuplicated) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-
-        Product product = new Product();
-        product.setId(request.getId());
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
-        productDB.add(product);
+        Product product = productService.createProduct(request);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri();
 
@@ -70,15 +47,7 @@ public class ProductController {
 //  PUT "/products/{id}"  update product data
     @PutMapping("/{id}")
     public ResponseEntity<Product> replaceProduct(@PathVariable("id") String id, @RequestBody Product request){
-        Optional<Product> productOp = productDB.stream().filter(p -> p.getId().equals(id)).findFirst();
-
-        if (!productOp.isPresent()){
-            return ResponseEntity.notFound().build();
-        }
-
-        Product product = productOp.get();
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
+        Product product = productService.replaceProduct(id, request);
 
         return ResponseEntity.ok().body(product);
     }
@@ -86,13 +55,10 @@ public class ProductController {
 //  DELETE "/products/{id}"  delete product data
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable("id") String id){
-        boolean isRemoved = productDB.removeIf(p -> p.getId().equals(id));
+        productService.deleteProduct(id);
 
-        if (isRemoved){
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.noContent().build();
+
     }
 
 //    search products
@@ -108,40 +74,11 @@ public class ProductController {
 //    GET "/products" get list of products
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(@ModelAttribute ProductQueryParameter param){
-        String nameKeyword = param.getKeyword();
-        String orderBy = param.getOrderBy();
-        String sortRule = param.getSortRule();
-
-        Comparator<Product> comparator = Objects.nonNull(orderBy)&&Objects.nonNull(sortRule)
-                ? configureSortComparator(orderBy,sortRule)
-                : (p1,p2) -> 0;
-
-        List<Product> products = productDB.stream()
-                .filter(p -> p.getName().toUpperCase().contains(nameKeyword.toUpperCase()))
-                .sorted(comparator)
-                .collect(Collectors.toList());
+        List<Product> products = productService.getProducts(param);
 
         return ResponseEntity.ok().body(products);
     }
 
-//    an instance of Comparator to set the sorting rule
-    private Comparator<Product> configureSortComparator(String orderBy, String sortRule){
-        Comparator<Product> comparator = (p1, p2) -> 0;
-
-        if (orderBy.equalsIgnoreCase("price")) {
-            comparator = Comparator.comparing(Product::getPrice);
-        } else if (orderBy.equalsIgnoreCase("name")) {
-//            colon is one way to call in lambda expression
-            comparator = Comparator.comparing(Product::getName);
-        }
-
-        if (sortRule.equalsIgnoreCase("desc")) {
-            comparator = comparator.reversed();
-        }
-
-        return comparator;
-
-    }
 
 }
 
---
+++ src/main/java/com/wei/spring_boot_practice/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.wei.spring_boot_practice.service;
+
+import com.wei.spring_boot_practice.entity.Product;
+import com.wei.spring_boot_practice.exception.ConflictException;
+import com.wei.spring_boot_practice.exception.NotFoundException;
+import com.wei.spring_boot_practice.prameter.ProductQueryParameter;
+import com.wei.spring_boot_practice.repository.MockProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ProductService {
+    @Autowired
+    private MockProductDAO productDAO;
+
+    public Product createProduct(Product request) {
+        boolean isIdDuplicated = productDAO.find(request.getId()).isPresent();
+        if (isIdDuplicated) {
+            throw new ConflictException("The id of the product is duplicated.");
+        }
+
+        Product product = new Product();
+        product.setId(request.getId());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+
+        return productDAO.insert(product);
+    }
+
+    public Product getProduct(String id) {
+        return productDAO.find(id).orElseThrow(() -> new NotFoundException("Can not find the product."));
+    }
+
+    public Product replaceProduct(String id, Product request) {
+        Product product = getProduct(id);
+        return productDAO.replace(product.getId(), request);
+    }
+
+    public void deleteProduct(String id) {
+        Product product = getProduct(id);
+        productDAO.delete(product.getId());
+    }
+
+    public List<Product> getProducts(ProductQueryParameter param) {
+        return productDAO.find(param);
+    }
+}
