237cad9b05887312b26154139f817a6ff1003b3e
brighten00 <brighten00@gmail.com>
2021-04-22 11:54:10 +0800

    add config file
    add config file

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # spring_boot_practice
-practice of spring 
 
-
+practice of spring
 
 jackson take reference on https://medium.com/chikuwa-tech-study/spring-boot-%E7%AC%AC12%E8%AA%B2-%E4%BD%BF%E7%94%A8jackson%E6%A8%99%E8%A8%98%E8%87%AA%E8%A8%82json%E6%AC%84%E4%BD%8D-60aa65fa3e4f
+
+if you want to add config information in application.properties
+
+key - value
+
+add "@configuration" before config class and use "${key}" in the class
+
+you can also specify which config to use while using JAR file
+
+reference : https://medium.com/chikuwa-tech-study/spring-boot-%E7%AC%AC13%E8%AA%B2-%E9%85%8D%E7%BD%AE%E6%87%89%E7%94%A8%E7%A8%8B%E5%BC%8F%E8%A8%AD%E5%AE%9A%E5%80%BC-1e25cf7c8e23
--- src/main/java/com/wei/spring_boot_practice/service/MailService.java
+++ src/main/java/com/wei/spring_boot_practice/service/MailService.java
@@ -1,6 +1,8 @@
 package com.wei.spring_boot_practice.service;
 
+import com.wei.spring_boot_practice.config.MailConfig;
 import com.wei.spring_boot_practice.entity.SendMailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -11,24 +13,28 @@ import java.util.Properties;
 
 @Service
 public class MailService {
+
+    @Autowired
+    private MailConfig mailConfig;
+
     public void sendMail(SendMailRequest request) {
-        final String host = "smtp.gmail.com";
-        final int port = 587;
-        final boolean enableAuth = true;
-        final boolean enableStarttls = true;
-        final String userAddress = "brighten.tso@omniwaresoft.com.tw";
-        final String pwd = "brighten95";
-        final String userDisplayName = "The one chasing the truth";
+//        final String host = "smtp.gmail.com";
+//        final int port = 587;
+//        final boolean enableAuth = true;
+//        final boolean enableStarttls = true;
+//        final String userAddress = "";
+//        final String pwd = "";
+//        final String userDisplayName = "The one chasing the truth";
 
         Properties props = new Properties();
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", port);
-        props.put("mail.smtp.auth", String.valueOf(enableAuth));
-        props.put("mail.smtp.starttls.enable", String.valueOf(enableStarttls));
+        props.put("mail.smtp.host", mailConfig.getHost());
+        props.put("mail.smtp.port", mailConfig.getPort());
+        props.put("mail.smtp.auth", String.valueOf(mailConfig.isAuthEnabled()));
+        props.put("mail.smtp.starttls.enable", String.valueOf(mailConfig.isStarttlsEnabled()));
 
         Session session = Session.getInstance(props, new Authenticator(){
             protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication(userAddress, pwd);
+                return new PasswordAuthentication(mailConfig.getUserAddress(), mailConfig.getUserPwd());
             }
         });
 
@@ -36,7 +42,7 @@ public class MailService {
             Message message = new MimeMessage(session);
             message.setSubject(request.getSubject());
             message.setContent(request.getContent(), "text/html; charset=UTF-8");
-            message.setFrom(new InternetAddress(userAddress, userDisplayName));
+            message.setFrom(new InternetAddress(mailConfig.getUserAddress(), mailConfig.getUserDisplayName()));
             for (String address : request.getReceivers()) {
                 message.addRecipient(Message.RecipientType.TO, new InternetAddress(address));
             }
